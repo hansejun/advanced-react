8b9a846ca1c9fc2d80c234727f05a84cb400e3a5
chacha <86880916+hansejun@users.noreply.github.com>
2024-03-02 23:23:45 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
-function App() {
-  return <></>;
-}
+import { useState, useEffect, useLayoutEffect, useRef } from 'react';
 
-export default App;
+const ResponsiveInput = ({ onClear, ...props }) => {
+  const el = useRef();
+  const [w, setW] = useState(0);
+  const measure = () => setW(el.current.offsetWidth);
+
+  useLayoutEffect(() => {
+    console.log('layout effect');
+    measure();
+  }, []);
+
+  // 1. 페인트 이후 실행된다.
+  // 2.
+  useEffect(() => {
+    console.log('effect');
+    performance.mark('effect:start');
+    window.addEventListener('resize', measure);
+    performance.mark('effect:end');
+    performance.measure('effect', 'effect:start', 'effect:end');
+    return () => window.removeEventListener('resize', measure);
+  }, []);
+
+  return (
+    <label>
+      <input {...props} ref={el} />
+      {w > 200 && <button onClick={onClear}>clear</button>}
+    </label>
+  );
+};
+
+export default function App() {
+  return <ResponsiveInput style={{ width: '50%' }} />;
+}
--- src/design-patterns/uncontrolled/app-container.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const AppContainer = () => {
-  return <div>AppContainer</div>;
-};
-
-export default AppContainer;
